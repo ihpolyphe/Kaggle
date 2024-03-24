9e90361a3878ac9549be8a58f66b7c65e52b0b10
ihpolyphe <ihpolyphe@gmail.com>
2024-03-24 10:31:25 +0900

    update

--- estimate_traffic/implement/sample_submit/src/predictor.py
+++ estimate_traffic/implement/sample_submit/src/predictor.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import pickle
+import tqdm
+from sklearn.preprocessing import LabelEncoder
+import os
 
 class ScoringService(object):
     @classmethod
@@ -14,20 +17,24 @@ class ScoringService(object):
         Returns:
             bool: The return value. True for success.
         """
+        model_path = os.path.join(model_path,"trained_model.pkl"))
 
         # カレントディレクトリにあるモデルデータの読み込み
         gbm = pickle.load(open(model_path, 'rb'))
+        print("gbm type:::{}".format(type(gbm)))
+        print("gbm:::{}".format(gbm))
 
+        prediction = cls.label_encode(inference_df)
 
         cls.model = gbm[0]
-        cls.data = inference_df
-        cls.log_pathes = inference_log
+        cls.data = prediction
+        cls.log_paths = inference_log
 
-        return True
+        return cls.model, cls.data, cls.log_paths
 
 
     @classmethod
-    def predict(cls, input, input_log):
+    def predict(cls,model, data, log_paths, input, input_log):
         """Predict method
 
         Args:
@@ -37,13 +44,38 @@ class ScoringService(object):
         Returns:
             prediction: Inference for the given input. Return columns must be ['datetime', 'start_code', 'end_code', 'KP'](DataFrame).
         """
-        print("input {}".format(input.info()))
-        print("input_log {}".format(input_log.info()))
-        prediction = input.copy()
-        prediction['weekday'] = pd.to_datetime(prediction['datetime'], format='%Y-%m-%d').apply(lambda x: x.weekday())
-        prediction['prediction'] = prediction['weekday'].apply(lambda x: 1 if x==4 or x==5 else 0)
-        predict = cls.model.predict(prediction)
-        print(predict)
-        prediction = prediction[['datetime', 'start_code', 'end_code', 'KP', 'prediction']]
+        # prediction = data.copy()
+        # prediction['weekday'] = pd.to_datetime(prediction['datetime'], format='%Y-%m-%d').apply(lambda x: x.weekday())
+        # prediction['prediction'] = prediction['weekday'].apply(lambda x: 1 if x==4 or x==5 else 0)
+        result = data.copy()
+        
+        cat_cols = ['road_code', 'start_code', 'end_code', 'section', 'direction', 'hour', 'dayofweek']
+        num_cols = ['year', 'month', 'day', 'search_specified', 'search_unspecified', 'KP', 'start_KP', 'end_KP', 'limit_speed']
+        feature_cols = cat_cols + num_cols
+        # 学習用特長量
+        prediction = data[feature_cols]
+        print(prediction)
+        # 推論結果
+        predict = model.predict(prediction)
 
-        return prediction
+        # print("prpedict result {}".format(pd.DataFrame(predict).describe()))
+
+        # 推論結果を元データへ追加
+        result['prediction'] = predict
+
+        # 提出データを作成
+        submit_data = result[['datetime', 'start_code', 'end_code', 'KP', 'prediction']]
+        return submit_data, pd.DataFrame(predict)
+
+    def label_encode(data):
+        cat_cols = ['road_code', 'start_code', 'end_code', 'section', 'direction', 'hour', 'dayofweek']
+        # ========================================
+        # カテゴリ変数の処理
+        # ========================================
+        le_dict = {}
+        for c in cat_cols:
+            le = LabelEncoder()
+            # print(data[c])
+            data[c] = le.fit_transform(data[c])
+            le_dict[c] = le
+        return data
